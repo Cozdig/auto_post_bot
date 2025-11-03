dcb508691a5fa61873e4c673d3ff6b1dea1c8ecd
Cozdig <tank-20@list.ru>
2025-11-03 17:04:39 +0300

    BOT IS READY

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from src.scheduler import PostScheduler
 async def main():
     all_info, last_row = get_info()
     bot = BotHandler(all_info)
-    test_scheduler = PostScheduler(all_info, last_row, bot)
-    await test_scheduler.run()
+    scheduler = PostScheduler(all_info, last_row, bot)
+    await scheduler.run()
 
 if __name__ == "__main__":
     asyncio.run(main())
